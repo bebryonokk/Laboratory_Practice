a8ebdf3a324892f0d1dd7af05983e9d3bc5d66ac
bebryonokk <overmarsianin@gmail.com>
2025-10-30 21:06:29 +0700

    a

--- Core/Scr/main.c
+++ Core/Scr/main.c
@@ -1,4 +1,15 @@
+#include "../../CMSIS/Devices/STM32F4xx/Inc/STM32F411xE/stm32f411xe.h"
+#include "../../CMSIS/Devices/STM32F4xx/Inc/stm32f4xx.h"
+
 int main(void)
 {
-    while(1){}
+    SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOAEN);
+    SET_BIT(GPIOA->MODER, GPIO_MODER_MODE5_0);
+    SET_BIT(GPIOA->OSPEEDR, GPIO_OSPEEDER_OSPEEDR5_1);
+    SET_BIT(GPIOA->BSRR, GPIO_BSRR_BR5);
+    
+    while(1)
+    {
+        SET_BIT(GPIOA->BSRR, GPIO_BSRR_BS5);
+    }
 }
