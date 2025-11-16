e0f8b3524af9ada9f18841b0c3a042e4b093cf0e
bebryonokk <overmarsianin@gmail.com>
2025-11-16 22:21:12 +0700

    lab1_add_task_comp

--- Core/Scr/init.c
+++ Core/Scr/init.c
@@ -8,31 +8,70 @@
 
 void Init_of_myself_macroses(void)
 {
-    RCC_AHB1EN |= RCC_GPIOAEN_EN;
-    RCC_GPIOA_MODER |= RCC_GPIOA_P4_OUT;
-    RCC_GPIOA_OTYPER |= RCC_GPIOA_OTYPER_P4_PP;
-    RCC_GPIOA_OSPEED |= RCC_GPIOA_OSPEED_P4_MED;
-    RCC_GPIOA_PUPDR |= RCC_GPIOA_PUPDR_P4_RST;
-    RCC_GPIOA_BSRR |= P4_LOW;
+    // Обращение через дерективы и собственноручно написанные макросы
+    SET_BIT(RCC_AHB1EN, RCC_GPIOAEN_EN);                // включение тактирования шины GPIOA
+    SET_BIT(RCC_GPIOA_MODER, RCC_GPIOA_P4_OUT);         // настройка режима работы порта A4 на вывод сигнала
+    SET_BIT(RCC_GPIOA_OTYPER, RCC_GPIOA_OTYPER_P4_PP);  // настройка типа выхода порта A4 на режим Output Push-Pull
+    SET_BIT(RCC_GPIOA_OSPEED, RCC_GPIOA_OSPEED_P4_MED); // настройка скорости работы порта A4 на среднюю
+    SET_BIT(RCC_GPIOA_PUPDR, RCC_GPIOA_PUPDR_P4_RST);   // настройка подтяжек/стяжек пина A4 на режим работы No Pull-Up, Pull-Down
+    SET_BIT(RCC_GPIOA_BSRR, P4_LOW);                    // задаем нулевой сигнал на порте перед началом работы
 }
 
 void Init_of_hand_writing(void)
 {
-    *(uint32_t *)(0x40023800UL + 0x30UL) |= 0x2UL;     // включение шины B
-    *(uint32_t *)(0x40020400UL + 0x00UL) |= 0x1UL;     // режим работы B0 на выход
-    *(uint32_t *)(0x40020400UL + 0x04UL) |= 0x0UL;     // двухтактный выход
-    *(uint32_t *)(0x40020400UL + 0x08UL) |= 0x1UL;     // скорость средняя
-    *(uint32_t *)(0x40020400UL + 0x0CUL) |= 0x0UL;     // выключение стяжек/подтяжек
-    *(uint32_t *)(0x40020400UL + 0x18UL) |= 0x10000UL; // выключение диода перед началом
+    // Прямое обращение к памяти
+    *(uint32_t *)(0x40023800UL + 0x30UL) |= 0x2UL;     // включение тактирования шины GPIOB
+    *(uint32_t *)(0x40020400UL + 0x00UL) |= 0x1UL;     // настройка режима работы порта B0 на вывод сигнала
+    *(uint32_t *)(0x40020400UL + 0x04UL) |= 0x0UL;     // настройка типа выхода порта B0 на режим Output Push-Pull
+    *(uint32_t *)(0x40020400UL + 0x08UL) |= 0x1UL;     // настройка скорости работы порта B0 на среднюю
+    *(uint32_t *)(0x40020400UL + 0x0CUL) |= 0x0UL;     // настраиваем подтяжки/стяжки пина B0 на режим No Pull-Up, Pull-Down
+    *(uint32_t *)(0x40020400UL + 0x18UL) |= 0x10000UL; // подаем нулевой сигнал на порт B0 перед началом работы
 }
 
 void Init_with_macroses(void)
 {
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;                                    // включение шины C
-    GPIOC->MODER |= GPIO_MODER_MODE0_0 | GPIO_MODER_MODE1_0;               // режим работы C0 и C1 на выход
+    // Использование библиотеки CMSIS
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;                     // включение тактирования шины GPIOB
+    GPIOC->MODER |= GPIO_MODER_MODE0_0 | GPIO_MODER_MODE1_0; // настройка режима работы порта B0 на вывод сигнала
     CLEAR_BIT(GPIOC->MODER, GPIO_MODER_MODE13);
     CLEAR_BIT(GPIOC->OTYPER, GPIO_OTYPER_OT0 + GPIO_OTYPER_OT1);           // двухтактный выход
     GPIOC->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR0_0 + GPIO_OSPEEDER_OSPEEDR1_0; // скорость средняя
     CLEAR_BIT(GPIOC->PUPDR, GPIO_PUPDR_PUPDR0 | GPIO_PUPDR_PUPDR1);        // выключение стяжек/подтяжек
     GPIOC->BSRR |= GPIO_BSRR_BR0 | GPIO_BSRR_BR1;                          // выключение диода перед началом
+}
+
+uint32_t but_hold_counter(void)
+{
+    uint32_t counter = 0;
+    while(READ_BIT(GPIOC->IDR, GPIO_IDR_ID13) == RESET)
+        {
+            counter++;  
+        }
+        return counter;
+}
+
+void Mode_and_diode_num(uint32_t counter)
+{
+        if(counter < 10000)
+        {
+            counter = 0;
+        }
+        if (counter > 10000 && counter < 300000)
+        {
+            but_check++;
+            counter = 0;
+        }
+        else if(counter > 10000)
+        {
+            herz++;
+            counter = 0;
+        }
+        if (but_check > 5)
+        {
+            but_check = 0;
+        }
+        if(herz > 3)
+        {
+            herz = 1;
+        }
 }
--- Core/Scr/main.c
+++ Core/Scr/main.c
@@ -1,35 +1,44 @@
 #include "../../CMSIS/Devices/STM32F4xx/Inc/STM32F411xE/stm32f411xe.h"
 #include "../../CMSIS/Devices/STM32F4xx/Inc/stm32f4xx.h"
-
-// #include "init.c"
+#include <math.h>
 #include "../Inc/init.h"
 int main(void)
 {
+    uint32_t counter = 0;
     Init_of_myself_macroses();
     Init_of_hand_writing();
     Init_with_macroses();
     int but_check = 0;
-    int pusher = 1;
-    int all_light = 0;
+    int herz = 1;
     while (1)
     {
-        if((((GPIOC->IDR) & GPIO_IDR_ID13) == 0) && (pusher == 1))
+        while(READ_BIT(GPIOC->IDR, GPIO_IDR_ID13) == RESET)
         {
-            pusher = 0;
+            counter++;  
         }
-        if ((((GPIOC->IDR) & GPIO_IDR_ID13) != 0) && (pusher == 0))
+        if(counter < 10000)
+        {
+            counter = 0;
+        }
+        if (counter > 10000 && counter < 300000)
         {
             but_check++;
-            all_light++;
-            pusher = 1;
-            int smth = 0;
-            for(int i = 0; i < 100000; i++)
-            {smth++;}
+            counter = 0;
         }
-        if (but_check > 4)
+        else if(counter > 10000)
+        {
+            herz++;
+            counter = 0;
+        }
+        if (but_check > 5)
         {
             but_check = 0;
         }
+        if(herz > 3)
+        {
+            herz = 1;
+        }
+        uint64_t delay = pow(herz, 3) * 25000;
         switch (but_check)
         {
         case 0:
@@ -38,36 +47,74 @@ int main(void)
             GPIOC->BSRR |= GPIO_BSRR_BR0 + GPIO_BSRR_BR1;
             break;
         case 1:
-            RCC_GPIOA_BSRR |= P4_HIGH;
             *(uint32_t *)(0x40020400UL + 0x18UL) |= 0x10000UL;
             GPIOC->BSRR |= GPIO_BSRR_BR0 + GPIO_BSRR_BR1;
+            for(uint32_t i = 0; i < delay; i++)
+            {
+                RCC_GPIOA_BSRR |= P4_HIGH;
+            }
+            for(uint32_t i = 0; i < delay; i++)
+            {
+                RCC_GPIOA_BSRR |= P4_LOW;
+            }
             break;
         case 2:
             RCC_GPIOA_BSRR |= P4_LOW;
             *(uint32_t *)(0x40020400UL + 0x18UL) |= 0x1UL;
             GPIOC->BSRR |= GPIO_BSRR_BR0 + GPIO_BSRR_BR1;
+              for(uint32_t i = 0; i < delay; i++)
+            {
+                *(uint32_t *)(0x40020400UL + 0x18UL) |= 0x1UL;
+            }
+            for(uint32_t i = 0; i < delay; i++)
+            {
+                *(uint32_t *)(0x40020400UL + 0x18UL) |= 0x10000UL;
+            }
             break;
         case 3:
             RCC_GPIOA_BSRR |= P4_LOW;
             *(uint32_t *)(0x40020400UL + 0x18UL) |= 0x10000UL;
             GPIOC->BSRR |= GPIO_BSRR_BR0 + GPIO_BSRR_BS1;
+              for(uint32_t i = 0; i < delay; i++)
+            {
+                GPIOC->BSRR |= GPIO_BSRR_BS1;
+            }
+            for(uint32_t i = 0; i < delay; i++)
+            {
+                GPIOC->BSRR |= GPIO_BSRR_BR1;
+            }
             break;
             break;
         case 4:
             RCC_GPIOA_BSRR |= P4_LOW;
             *(uint32_t *)(0x40020400UL + 0x18UL) |= 0x10000UL;
             GPIOC->BSRR |= GPIO_BSRR_BS0 + GPIO_BSRR_BR1;
+              for(uint32_t i = 0; i < delay; i++)
+            {
+                GPIOC->BSRR |= GPIO_BSRR_BS0;
+            }
+            for(uint32_t i = 0; i < delay; i++)
+            {
+                GPIOC->BSRR |= GPIO_BSRR_BR0;
+            }
             break;
-        }
-        if (all_light == 5)
-        {
+        case 5:
             *(uint32_t *)(0x40020400UL + 0x18UL) |= 0x1UL;
             RCC_GPIOA_BSRR |= P4_HIGH;
             GPIOC->BSRR |= GPIO_BSRR_BS0 + GPIO_BSRR_BS1;
-        }
-        if (all_light > 5)
-        {
-            all_light = 0;
+              for(uint32_t i = 0; i < delay; i++)
+            {
+                *(uint32_t *)(0x40020400UL + 0x18UL) |= 0x1UL;
+            RCC_GPIOA_BSRR |= P4_HIGH;
+            GPIOC->BSRR |= GPIO_BSRR_BS0 + GPIO_BSRR_BS1;
+            }
+            for(uint32_t i = 0; i < delay; i++)
+            {
+                RCC_GPIOA_BSRR |= P4_LOW;
+            *(uint32_t *)(0x40020400UL + 0x18UL) |= 0x10000UL;
+            GPIOC->BSRR |= GPIO_BSRR_BR0 + GPIO_BSRR_BR1;
+            }
+            break;
         }
     }
 }
