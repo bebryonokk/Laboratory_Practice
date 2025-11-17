c77d754027e6f790a446d53bbb7a8351898f83f5
bebryonokk <overmarsianin@gmail.com>
2025-11-18 01:34:16 +0700

    lab1_comp

--- Core/Scr/main.c
+++ Core/Scr/main.c
@@ -1,14 +1,15 @@
-
 #include "../Inc/init.h"
 
 uint8_t but_check = 0;
 uint8_t pusher = 0;
+uint8_t first_led = 0;
+uint8_t second_led = 0;
+uint8_t third_led = 0;
+uint8_t fourth_led = 0;
 
 int main(void)
 {
-    Init_of_myself_macroses();
-    Init_of_hand_writing();
-    Init_with_macroses();
+    GPIO_INIT();
     while (1)
     {
         if ((READ_BIT(GPIOC->IDR, GPIO_IDR_ID13) == RESET) && (pusher == 0))
@@ -21,5 +22,47 @@ int main(void)
             pusher = 0;
         }
         main_process(but_check);
+        if (but_check == 1)
+        {
+            first_led = 1;
+            second_led = 0;
+            third_led = 0;
+            fourth_led = 0;
+        }
+        if (but_check == 2)
+        {
+            first_led = 0;
+            second_led = 1;
+            third_led = 0;
+            fourth_led = 0;
+        }
+        if (but_check == 3)
+        {
+            first_led = 0;
+            second_led = 0;
+            third_led = 1;
+            fourth_led = 0;
+        }
+        if (but_check == 4)
+        {
+            first_led = 0;
+            second_led = 0;
+            third_led = 0;
+            fourth_led = 1;
+        }
+        if (but_check == 5)
+        {
+            first_led = 1;
+            second_led = 1;
+            third_led = 1;
+            fourth_led = 1;
+        }
+        else
+        {
+            first_led = 0;
+            second_led = 0;
+            third_led = 0;
+            fourth_led = 0;
+        }
     }
-}
+}   
